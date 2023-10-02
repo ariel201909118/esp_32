815f0904e3720d985b5672c287b64e4e79a02958
ariel201909118 <79768095+ariel201909118@users.noreply.github.com>
2023-10-02 17:40:54 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 let keys = document.getElementsByClassName("btn");
 const btn_keys = Array.from(keys);
-
+let data_to_send = null;
 /*key pad reading*/
 btn_keys.forEach(
     (key)=>{
         key.addEventListener("click",function (){
-            console.log("Key pressed "+ this.innerText)
+            data_to_send = this.innerText;
+            console.log("Key pressed "+ data_to_send);
+            fetch("http://192.168.2.210:80/data",{
+                method:'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(data_to_send)
+            })
+            .then(response => response.json())
+            .then(data => {console.log(data)})
         })
 })
